3d85a6a2de7a9e95b4b26ea111af45d645bd143a
alaqsaka <aqsaabidin15@gmail.com>
2022-11-19 16:31:05 +0700

    first commit

---
+++ src/api/albums/handler.js
@@ -0,0 +1,60 @@
+const ClientError = require("../../exceptions/ClientError");
+
+class AlbumsHandler {
+  constructor(service, validator) {
+    this._service = service;
+    this._validator = validator;
+
+    this.postAlbumHandler = this.postAlbumHandler.bind(this);
+    this.getAlbumsHandler = this.getAlbumsHandler.bind(this);
+  }
+
+  async postAlbumHandler(request, h) {
+    try {
+      this._validator.validateAlbumPayload(request.payload);
+      const { name = "untitled", year } = request.payload;
+
+      const albumId = await this._service.addAlbum({ name, year });
+
+      const response = h.response({
+        status: "success",
+        message: "Menamabahkan Album",
+        data: {
+          albumId,
+        },
+      });
+      response.code(201);
+      return response;
+    } catch (error) {
+      if (error instanceof ClientError) {
+        const response = h.response({
+          status: "fail",
+          message: error.message,
+        });
+        response.code(error.statusCode);
+        return response;
+      }
+
+      // Server ERROR!
+      const response = h.response({
+        status: "error",
+        message: "Maaf, terjadi kegagalan pada server kami.",
+      });
+      response.code(500);
+      console.error(error);
+      return response;
+    }
+  }
+
+  async getAlbumsHandler() {
+    const albums = await this._service.getAlbums();
+    return {
+      status: "success",
+      data: {
+        albums,
+      },
+    };
+  }
+}
+
+module.exports = AlbumsHandler;
---
+++ src/services/postgres/AlbumsService.js
@@ -0,0 +1,36 @@
+const { nanoid } = require("nanoid");
+const { Pool } = require("pg");
+const InvariantError = require("../../exceptions/InvariantError");
+const { mapDBToModelAlbum } = require("../../utils");
+
+class AlbumsService {
+  constructor() {
+    this._pool = new Pool();
+  }
+
+  async addAlbum({ name, year }) {
+    const id = nanoid(16);
+    const createdAt = new Date().toISOString();
+    const updatedAt = createdAt;
+
+    const query = {
+      text: "INSERT INTO albums VALUES($1, $2, $3, $4, $5) RETURNING id",
+      values: [id, name, year, createdAt, updatedAt],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rows[0].id) {
+      throw new InvariantError("Album gagal ditambahkan");
+    }
+
+    return result.rows.map(mapDBToModelAlbum)[0].id;
+  }
+
+  async getAlbums() {
+    const result = await this._pool.query("SELECT * FROM albums");
+    return result.rows.map(mapDBToModelAlbum);
+  }
+}
+
+module.exports = AlbumsService;
