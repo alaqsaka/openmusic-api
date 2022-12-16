520f115fd05662d72ee70916deead71bacad5a24
alaqsaka <aqsaabidin15@gmail.com>
2022-12-16 22:18:26 +0700

    chore: delete song by id

--- src/api/songs/handler.js
+++ src/api/songs/handler.js
@@ -9,6 +9,7 @@ class SongsHandler {
     this.getSongsHandler = this.getSongsHandler.bind(this);
     this.getSongByIdHandler = this.getSongByIdHandler.bind(this);
     this.putSongByIdHandler = this.putSongByIdHandler.bind(this);
+    this.deleteSongByIdHandler = this.deleteSongByIdHandler.bind(this);
   }
 
   async postSongHandler(request, h) {
@@ -137,6 +138,36 @@ class SongsHandler {
       }
 
       // Server ERROR!
+      const response = h.response({
+        status: "error",
+        message: "Maaf, terjadi kegagalan pada server kami.",
+      });
+      response.code(500);
+      console.error(error);
+      return response;
+    }
+  }
+
+  async deleteSongByIdHandler(request, h) {
+    try {
+      const { id } = request.params;
+
+      await this._service.deleteSongById(id);
+
+      return {
+        status: "success",
+        message: "Song berhasil dihapus",
+      };
+    } catch (error) {
+      if (error instanceof ClientError) {
+        const response = h.response({
+          status: "fail",
+          message: error.message,
+        });
+        response.code(error.statusCode);
+        return response;
+      }
+
       const response = h.response({
         status: "error",
         message: "Maaf, terjadi kegagalan pada server kami.",
