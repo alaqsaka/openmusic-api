6b86ed773305f6c97beefac87e4d5e8f96f92b64
alaqsaka <aqsaabidin15@gmail.com>
2022-12-24 11:56:01 +0700

    init routes, create validator, service, handler files, create api for add user

--- src/server.js
+++ src/server.js
@@ -1,18 +1,34 @@
 require("dotenv").config();
 const Hapi = require("@hapi/hapi");
+
+// albums
 const albums = require("./api/albums");
 const AlbumsService = require("./services/postgres/AlbumsService");
 const AlbumsValidator = require("./validator/album");
+
+// songs
 const songs = require("./api/songs");
 const SongsService = require("./services/postgres/SongsService");
 const SongsValidator = require("./validator/song");
 
+// users
+const users = require("./api/users");
+const UsersService = require("./services/postgres/UsersService");
+const UsersValidator = require("./validator/users");
+
 const init = async () => {
   const songsService = new SongsService();
   const albumsService = new AlbumsService();
+  const usersService = new UsersService();
+
   const server = Hapi.server({
     port: process.env.PORT,
     host: process.env.HOST,
+    routes: {
+      cors: {
+        origin: ["*"],
+      },
+    },
   });
 
   await server.register({
@@ -28,6 +44,14 @@ const init = async () => {
     options: {
       service: songsService,
       validator: SongsValidator,
+    },
+  });
+
+  await server.register({
+    plugin: users,
+    options: {
+      service: usersService,
+      validator: UsersValidator,
     },
   });
 
